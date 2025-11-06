1ec1be1bff465b8b1ab0d142642a2d1f71df7766
anujsharma121 <anuj.sharma@unoapp.io>
2025-11-06 12:26:19 +0530

    new vercel setup

--- server/api/wsgi.py
+++
@@ -1,6 +0,0 @@
-# server/api/wsgi.py
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
-app = get_wsgi_application()   # Vercel looks for `app`
